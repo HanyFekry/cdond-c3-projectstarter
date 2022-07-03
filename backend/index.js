54c35978cb3b2dd321eaaf714fd62c548cc9ad99
hany <hanyaly_82@hotmail.com>
2022-07-03 15:13:38 +0000

    ubuntu update

--- backend/index.js
+++
@@ -1,6 +0,0 @@
-var http = require("http");
-var server = http.createServer(function (req, res) {
-    res.writeHead(200);
-    res.end("Hello world from Hany node app!");
-});
-server.listen(3030);
